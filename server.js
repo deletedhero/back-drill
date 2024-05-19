97c262c60724f3e71096ffcb8450b7e40bcaa7c2
Sakpal Amit <amitsakpal744@gmail.com>
2024-05-19 15:18:22 +0530

    added env and users in serverjs for demo fetch

--- server.js
+++ server.js
@@ -8,7 +8,32 @@ app.get('/',(req , res) => {
     res.send(`server is runnning on port ${port}`)
 })
 
+// USERS //
 
+app.get('/api/users',(req,res) => {
+   users =  [
+        {
+          "id": 1,
+          "name": "Sakpal Amit",
+          "age": 19,
+          "role": "backend"
+        },
+        {
+          "id": 2,
+          "name": "Darshan dhakan",
+          "age": 21,
+          "role": "frontend"
+        },
+        {
+          "id": 3,
+          "name": "Dhiren bhandare",
+          "age": 26,
+          "role": "fullstack"
+        }
+      ]
+
+      res.send(users)
+})
 
 app.listen(port,()=> {
     console.log(`server is running on port ${port}`)
